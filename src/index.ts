9252ad1b540ca3d1a003df3ba0bf4bbfaf87f18f
b_damlabin <b_damlabin@yahoo.com>
2022-08-17 06:45:05 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,8 @@ const symbolTable5 = new Map();
 
 const symbolTable6 = new Map();
 
+const symbolTable7 = new Map();
+
 const typeTable = new Map();
 
 const data = fs.readFileSync('src/data/graph.onto', 'utf8');
@@ -56,13 +58,23 @@ for (const line of  lines) {
     symbolTable6.set(words[0], {_type: words[2]});
   }
 
+  if (words[1] === 'has-receipt') {
+    symbolTable7.set(words[0], {_type: words[2]});
+  }
+
 }
 
-let transitionNode;
 let transitionNode1;
 let transitionNode2;
 let transitionNode3;
-let transitionNode4;
+let src1;
+let src2;
+let src3;
+let tgt1;
+let tgt2;
+let tgt3;
+let tgt4;
+let tgt5;
 
 const G = digraph('G', (g) => {
   for(const [key, value] of symbolTable.entries()) {
@@ -73,8 +85,16 @@ const G = digraph('G', (g) => {
 
         if(symbolTable.get(typeTable.get(key)._type)._type === 'vendor') {
           g.node(typeTable.get(key)._type);
-          transitionNode1 = typeTable.get(key)._type;
+          src1 = typeTable.get(key)._type;
         }
+
+      }
+
+      if(typeTable.get(key)._type === 'cashier') {
+
+          g.node(typeTable.get(key)._type);
+          src3 = typeTable.get(key)._type;
+          tgt5 = typeTable.get(key)._type;
 
       }
 
@@ -97,7 +117,7 @@ const G = digraph('G', (g) => {
 
           if(symbolTable.get(type1)._type === 'client' && symbolTable.get(type2)._type === 'item') {
             g.node(type1 + " " + type2);
-            transitionNode2 = type1 + " " + type2;
+            src2 = type1 + " " + type2;
           }
 
         }
@@ -123,7 +143,7 @@ const G = digraph('G', (g) => {
 
           if(symbolTable.get(type1)._type === 'vendor' && symbolTable.get(type2)._type === 'item') {
             g.node(type1 + " " + type2);
-            transitionNode3 = type1 + " " + type2;
+            tgt1 = type1 + " " + type2;
           }
 
         }
@@ -149,7 +169,59 @@ const G = digraph('G', (g) => {
 
           if(symbolTable.get(type1)._type === 'client' && symbolTable.get(type2)._type === 'money') {
             g.node(type1 + " " + type2);
-            transitionNode4 = type1 + " " + type2;
+            tgt2 = type1 + " " + type2;
+          }
+
+        }
+
+      }
+
+      if(typeTable.get(key)._type === 't4') {
+
+        if(symbolTable.get(typeTable.get(key)._type)._type === 'tuple') {
+
+          let type1;
+          let type2;
+
+          if(symbolTable1.get(typeTable.get(key)._type)._type === 'Alice') {
+            type1 = symbolTable1.get(typeTable.get(key)._type)._type; 
+            console.log(type1);
+          }
+
+          if(symbolTable7.get(typeTable.get(key)._type)._type === 'receipt') {
+            type2 = symbolTable7.get(typeTable.get(key)._type)._type; 
+            console.log(type2);
+          }
+
+          if(symbolTable.get(type1)._type === 'client' && symbolTable.get(type2)._type === 'pays') {
+            g.node(type1 + " " + type2);
+            tgt3 = type1 + " " + type2;
+          }
+
+        }
+
+      }
+
+      if(typeTable.get(key)._type === 't5') {
+
+        if(symbolTable.get(typeTable.get(key)._type)._type === 'tuple') {
+
+          let type1;
+          let type2;
+
+          if(symbolTable3.get(typeTable.get(key)._type)._type === 'V1') {
+            type1 = symbolTable3.get(typeTable.get(key)._type)._type; 
+            console.log(type1);
+          }
+
+          if(symbolTable2.get(typeTable.get(key)._type)._type === 'packed-shirt') {
+            type2 = symbolTable2.get(typeTable.get(key)._type)._type; 
+            console.log(type2);
+          }
+
+          if(symbolTable.get(type1)._type === 'vendor' && symbolTable.get(type2)._type === 'item') {
+            g.node(type1 + " " + type2);
+            tgt4 = type1 + " " + type2;
           }
 
         }
@@ -159,11 +231,27 @@ const G = digraph('G', (g) => {
 
     }
 
-    if(value._type === 'transition') {
+    if(value._type === 'transition-1') {
 
       console.log(key);
 
-      transitionNode = g.node(key, {[attribute.shape]: 'box'})
+      transitionNode1 = g.node(key, {[attribute.shape]: 'box'})
+
+    }
+
+    if(value._type === 'transition-2') {
+
+      console.log(key);
+
+      transitionNode2 = g.node(key, {[attribute.shape]: 'box'})
+
+    }
+
+    if(value._type === 'transition-3') {
+
+      console.log(key);
+
+      transitionNode3 = g.node(key, {[attribute.shape]: 'box'})
 
     }
 
@@ -172,15 +260,15 @@ const G = digraph('G', (g) => {
       if(key === 'f1') {
 
         if(symbolTable5.get(key)._type === 'vendor-available-1' && symbolTable6.get(key)._type === 'take-home') {
-          g.edge([transitionNode1, transitionNode]);
+          g.edge([src1, transitionNode1]);
         }
 
       }
 
       if(key === 'f2') {
 
-        if(symbolTable5.get(key)._type === 'take-home' && symbolTable6.get(key)._type === 'alice-with-50-EUR-4') {
-          g.edge([transitionNode, transitionNode4]);
+        if(symbolTable5.get(key)._type === 'take-home' && symbolTable6.get(key)._type === 'client-with-50-EUR-4') {
+          g.edge([transitionNode1, tgt2]);
         }
 
       }
@@ -188,7 +276,7 @@ const G = digraph('G', (g) => {
       if(key === 'f3') {
 
         if(symbolTable5.get(key)._type === 'client-with-item-2' && symbolTable6.get(key)._type === 'take-home') {
-          g.edge([transitionNode, transitionNode3]);
+          g.edge([transitionNode1, tgt1]);
         }
 
       }
@@ -196,7 +284,55 @@ const G = digraph('G', (g) => {
       if(key === 'f4') {
 
         if(symbolTable5.get(key)._type === 'take-home' && symbolTable6.get(key)._type === 'vendor-with-item-3') {
-          g.edge([transitionNode2, transitionNode]);
+          g.edge([src2, transitionNode1]);
+        }
+
+      }
+
+      if(key === 'f5') {
+
+        if(symbolTable5.get(key)._type === 'vendor-with-item-3' && symbolTable6.get(key)._type === 'packs-shirt') {
+          g.edge([tgt1, transitionNode2]);
+        }
+
+      }
+
+      if(key === 'f6') {
+
+        if(symbolTable5.get(key)._type === 'packs-shirt' && symbolTable6.get(key)._type === 'vendor-with-packed-item-7') {
+          g.edge([transitionNode2, tgt4]);
+        }
+
+      }
+
+      if(key === 'f7') {
+
+        if(symbolTable5.get(key)._type === 'client-with-50-EUR-4' && symbolTable6.get(key)._type === 'pays-take-home') {
+          g.edge([tgt2, transitionNode3]);
+        }
+
+      }
+
+      if(key === 'f8') {
+
+        if(symbolTable5.get(key)._type === 'pays-take-home' && symbolTable6.get(key)._type === 'cashier-available-5') {
+          g.edge([transitionNode3, tgt5]);
+        }
+
+      }
+
+      if(key === 'f9') {
+
+        if(symbolTable5.get(key)._type === 'cashier-available-5' && symbolTable6.get(key)._type === 'pays-take-home') {
+          g.edge([src3, transitionNode3]);
+        }
+
+      }
+
+      if(key === 'f10') {
+
+        if(symbolTable5.get(key)._type === 'pays-take-home' && symbolTable6.get(key)._type === 'client-with-receipt-6') {
+          g.edge([transitionNode3, tgt3]);
         }
 
       }
